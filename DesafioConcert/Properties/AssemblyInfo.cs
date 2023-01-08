f771a6cc0965b01c25d36300a30c9d7e142bd2ef
Maria Helena Gomes <mariahelenadeabreucsc@gmail.com>
2023-01-07 22:49:58 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioConcert/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesafioConcert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesafioConcert")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e848cf56-8c0f-4216-be09-86d766b6d6c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DesafioConcert/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Google.Testes.Buscador
+{
+    [TestClass]
+    public class BuscadorGoogle
+    {
+        private static IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private static string baseURL;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+        }
+
+        [ClassCleanup]
+        public static void CleanupClass()
+        {
+            try
+            {
+                driver.Close();
+                driver.Dispose();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [TestMethod]
+        public void PesquisaTextoConcert()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Name("q")).Click();
+            driver.FindElement(By.Name("q")).Clear();
+            driver.FindElement(By.Name("q")).SendKeys("concert technologies");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            Assert.IsNotNull(driver.FindElement(By.TagName("h3")).Equals("Concert Technologies | Tecnologia e Inovação para o seu ..."));
+        }
+   
+        [TestMethod]
+        public void PesquisaEstouComSorte()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Name("q")).Click();
+            driver.FindElement(By.Name("q")).Clear();
+            driver.FindElement(By.Name("q")).SendKeys("concert technologies site o");
+            driver.FindElement(By.XPath("//div[4]/center/input[2]")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            Assert.AreEqual("https://www.concert.com.br/", driver.Url);
+        }
+     
+        [TestMethod]
+        public void PesquisaPalavraEscritaIncorretamente()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).Click();
+            driver.FindElement(By.Name("q")).Clear();
+            driver.FindElement(By.Name("q")).SendKeys("A melhor telinologia com c");
+            driver.FindElement(By.XPath("//div[4]/center/input")).Click();
+
+            Assert.IsNotNull(driver.FindElement(By.XPath("//a[@id='fprsl']/b/i")).Equals("A melhor tecnologia com c"));
+
+        }
+
+    }
+}
